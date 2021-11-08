958ee21c8e4d0779119db90d67755a2685fc42fc
therodycc <therodycc@gmail.com>
2021-11-07 23:17:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+// components
+import { chat } from "./components/chat";
+import { join } from "./components/join";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={join} />
+      <Route path="/chat" exact component={chat} />
+    </Router>
   );
 }
 
---
+++ src/components/join.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const join = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
